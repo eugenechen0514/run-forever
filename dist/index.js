5d8d2e39b9d027098ca59660dcebe744fe495546
EugeneChen <yujiechen0514@gmail.com>
2019-09-20 15:40:14 +0800

    change version

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.END = Symbol('run-forever');
-function forever(fn, previousValue, failCallback = (e) => { }) {
-    setImmediate((_fn, _previousValue, _failCallback) => {
+function forever(fn, previousValue, callback = () => { }) {
+    setImmediate((_fn, _previousValue, _callback) => {
         _fn(_previousValue)
             .then((newValue) => {
             if (newValue !== exports.END) {
-                forever(_fn, newValue, _failCallback);
+                forever(_fn, newValue, _callback);
+            }
+            else {
+                _callback(null, newValue);
             }
         })
             .catch((e) => {
-            _failCallback(e);
+            _callback(e, _previousValue);
         });
-    }, fn, previousValue, failCallback);
+    }, fn, previousValue, callback);
 }
 exports.forever = forever;
+function foreverPromise(fn, previousValue) {
+    return new Promise((resolve, reject) => {
+        forever(fn, previousValue, (e, value) => {
+            if (e) {
+                reject(e);
+            }
+            else {
+                resolve(value);
+            }
+        });
+    });
+}
+exports.foreverPromise = foreverPromise;
 //# sourceMappingURL=index.js.map
