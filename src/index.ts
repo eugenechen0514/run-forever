efbe7358727b2e55fa10069353ea62dcd9355c96
EugeneChen <yujiechen0514@gmail.com>
2023-04-26 11:11:49 +0800

    fix: fix types

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ export const END = Symbol('run-forever');
 
 export type ForeverControlValue<T = any> = T | Symbol;
 export type ForeverExecutionFunction<T = ForeverControlValue> = (previousValue?: T) => Promise<ForeverControlValue<T>>;
-export type ForeverCallback = (e?: Error, value?: ForeverControlValue) => void;
+export type ForeverCallback = (e?: Error | undefined, value?: ForeverControlValue) => void;
 
 export function forever<T = ForeverControlValue>(fn: ForeverExecutionFunction, previousValue?: T, callback: ForeverCallback = () => {}): void {
-	setImmediate((_fn, _previousValue, _callback) => {
+	setImmediate((_fn: ForeverExecutionFunction, _previousValue: T, _callback: ForeverCallback) => {
 		_fn(_previousValue)
 			.then((newValue: ForeverControlValue) => {
 				if(newValue !== END) {
