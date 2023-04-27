32ca1be43f6af6e1ffc1d15f0683609ad1c9746d
EugeneChen <yujiechen0514@gmail.com>
2023-04-27 09:54:49 +0800

    release: 0.3.8 fix types and ts env fix README.md fix: pass value in callback

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,5 +3,5 @@ export type ForeverControlValue<T = undefined> = T | Symbol;
 export type ForeverPreviousValue<T = undefined> = Exclude<T, Symbol>;
 export type ForeverExecutionFunction<T = undefined> = (previousValue: ForeverPreviousValue<T>) => Promise<ForeverControlValue<T>>;
 export type ForeverCallback<T = undefined> = (e: Error | undefined, value: ForeverPreviousValue<T>) => void;
-export declare function forever<INIT_V = undefined>(fn: ForeverExecutionFunction<INIT_V>, previousValue?: (INIT_V | undefined), callback?: ForeverCallback<INIT_V>): void;
+export declare function forever<INIT_V = undefined>(fn: ForeverExecutionFunction<INIT_V>, previousValue: INIT_V, callback?: ForeverCallback<INIT_V>): void;
 export declare function foreverPromise<INIT_V = undefined>(fn: ForeverExecutionFunction<INIT_V>, previousValue: INIT_V): Promise<ForeverPreviousValue<INIT_V>>;
